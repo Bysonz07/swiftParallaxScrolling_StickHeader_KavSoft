ddb6dd9c88912b046955649984b491301a4ab894
DifaNaufalPratama <dnpn07@gmail.com>
2021-10-26 07:49:50 +0700

    Initial Commit

---
+++ ParallaxScrolling_StickyHeader/ParallaxScrolling_StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxScrolling_StickyHeaderApp.swift
+//  ParallaxScrolling_StickyHeader
+//
+//  Created by Difa N Pratama on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxScrolling_StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
